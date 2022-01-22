8ca6ef8ee0c0cf46322e8117e66734c260f04756
kristina <dudjakkristina@gmail.com>
2022-01-22 18:51:22 +0100

    Set up main destinations

--- app/src/main/java/com/example/maternityrisk/MainActivity.kt
+++ app/src/main/java/com/example/maternityrisk/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.maternityrisk
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.entry).setOnClickListener {
+            val intent = Intent(this, EntryActivity::class.java)
+            startActivity(intent)
+        }
+        findViewById<Button>(R.id.history).setOnClickListener {
+            val intent = Intent(this, HistoryActivity::class.java)
+            startActivity(intent)
+        }
+        findViewById<Button>(R.id.statistics).setOnClickListener {
+            val intent = Intent(this, StatisticsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
